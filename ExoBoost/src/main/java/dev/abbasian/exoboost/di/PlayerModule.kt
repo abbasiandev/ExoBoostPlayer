a12f84e70d86e1e3dc04bbb1a91ba284a716a2ee
Abbasian.dev <mam-1371@outlook.com>
2025-09-19 19:22:41 +0330

    add dependency injection module for video player components

---
+++ ExoBoost/src/main/java/dev/abbasian/exoboost/di/PlayerModule.kt
@@ -0,0 +1,32 @@
+package dev.abbasian.exoboost.di
+
+import androidx.media3.common.util.UnstableApi
+import dev.abbasian.exoboost.data.manager.ExoPlayerManager
+import dev.abbasian.exoboost.data.repository.VideoRepositoryImpl
+import dev.abbasian.exoboost.domain.repository.VideoRepository
+import dev.abbasian.exoboost.domain.usecase.CacheVideoUseCase
+import dev.abbasian.exoboost.domain.usecase.PlayVideoUseCase
+import dev.abbasian.exoboost.domain.usecase.RetryVideoUseCase
+import dev.abbasian.exoboost.presentation.viewmodel.VideoPlayerViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+@UnstableApi
+val playerModule = module {
+    single { ExoPlayerManager(androidContext(), get(), get()) }
+
+    single<VideoRepository> { VideoRepositoryImpl(get(), get()) }
+
+    factory { PlayVideoUseCase(get()) }
+    factory { CacheVideoUseCase(get()) }
+    factory { RetryVideoUseCase(get()) }
+
+    viewModel {
+        VideoPlayerViewModel(
+            playVideoUseCase = get(),
+            cacheVideoUseCase = get(),
+            retryVideoUseCase = get()
+        )
+    }
+}
