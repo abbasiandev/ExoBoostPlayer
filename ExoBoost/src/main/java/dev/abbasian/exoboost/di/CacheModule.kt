dd903eaf2f2867b01736976cb3a2a1e34301757e
Abbasian.dev <mam-1371@outlook.com>
2025-09-19 19:16:39 +0330

    inject cash manager + factory

---
+++ ExoBoost/src/main/java/dev/abbasian/exoboost/di/CacheModule.kt
@@ -0,0 +1,24 @@
+package dev.abbasian.exoboost.di
+
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.cache.Cache
+import androidx.media3.datasource.cache.CacheDataSource
+import dev.abbasian.exoboost.data.manager.CacheManager
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+@UnstableApi
+val cacheModule = module {
+    single { CacheManager(androidContext()) }
+
+    single<Cache> { get<CacheManager>().getCache() }
+
+    factory<DataSource.Factory> {
+        CacheDataSource.Factory()
+            .setCache(get())
+            .setUpstreamDataSourceFactory(get<androidx.media3.datasource.HttpDataSource.Factory>())
+            .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
+            .setCacheWriteDataSinkFactory(null)
+    }
+}
