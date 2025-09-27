3e729d66901170358b94079c980ac9a8a3337b33
Abbasian.dev <mam-1371@outlook.com>
2025-09-27 21:56:10 +0330

    add data module for inject with koin

---
+++ ExoBoost/src/main/java/dev/abbasian/exoboost/di/DataModule.kt
@@ -0,0 +1,8 @@
+package dev.abbasian.exoboost.di
+
+import dev.abbasian.exoboost.data.store.EqualizerPreferencesManager
+import org.koin.dsl.module
+
+val dataModule = module {
+    single<EqualizerPreferencesManager> { EqualizerPreferencesManager(get()) }
+}
