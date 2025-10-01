a2f966caabf6a114b01b3a0ae2c08d0d5ce0dc9e
Abbasian.dev <mam-1371@outlook.com>
2025-10-02 00:24:41 +0330

    refactor player module to inject recovery manager

--- ExoBoost/src/main/java/dev/abbasian/exoboost/di/PlayerModule.kt
+++ ExoBoost/src/main/java/dev/abbasian/exoboost/di/PlayerModule.kt
@@ -1,6 +1,7 @@
 package dev.abbasian.exoboost.di
 
 import androidx.media3.common.util.UnstableApi
+import dev.abbasian.exoboost.data.manager.AutoRecoveryManager
 import dev.abbasian.exoboost.data.manager.ExoPlayerManager
 import dev.abbasian.exoboost.data.repository.MediaRepositoryImpl
 import dev.abbasian.exoboost.domain.repository.MediaRepository
@@ -14,7 +15,9 @@ import org.koin.dsl.module
 
 @UnstableApi
 val playerModule = module {
-    single { ExoPlayerManager(androidContext(), get(), get(), get()) }
+    single { AutoRecoveryManager(get()) }
+
+    single { ExoPlayerManager(androidContext(), get(), get(), get(), get()) }
 
     single<MediaRepository> { MediaRepositoryImpl(get(), get()) }
 
