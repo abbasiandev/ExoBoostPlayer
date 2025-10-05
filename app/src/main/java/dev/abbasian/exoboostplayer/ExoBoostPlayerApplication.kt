304957e82f02a13a571d11e6666cad4c1fa653c5
Abbasian.dev <mam-1371@outlook.com>
2025-10-05 20:47:53 +0330

    no need load app module

--- app/src/main/java/dev/abbasian/exoboostplayer/ExoBoostPlayerApplication.kt
+++ app/src/main/java/dev/abbasian/exoboostplayer/ExoBoostPlayerApplication.kt
@@ -2,13 +2,10 @@ package dev.abbasian.exoboostplayer
 
 import android.app.Application
 import dev.abbasian.exoboost.ExoBoost
-import dev.abbasian.exoboostplayer.di.appModule
-import org.koin.core.context.loadKoinModules
 
 class ExoBoostPlayerApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         ExoBoost.initialize(this)
-        loadKoinModules(appModule)
     }
 }
