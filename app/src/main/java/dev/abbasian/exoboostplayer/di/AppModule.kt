f9503c453e5b9d10e0a273234f137a61751869f7
Abbasian.dev <mam-1371@outlook.com>
2025-10-06 11:38:23 +0330

    removed because unused further

--- app/src/main/java/dev/abbasian/exoboostplayer/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package dev.abbasian.exoboostplayer.di
-
-import dev.abbasian.exoboostplayer.presentation.viewmodel.MainViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val appModule = module {
-    viewModel { MainViewModel() }
-}
