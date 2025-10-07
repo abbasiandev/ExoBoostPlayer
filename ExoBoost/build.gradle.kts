7ad441e27447c16a71d3a739cd220e6b6d2ad751
Abbasian.dev <mam-1371@outlook.com>
2025-10-07 18:21:32 +0330

    bump version to 1.0.1

--- ExoBoost/build.gradle.kts
+++ ExoBoost/build.gradle.kts
@@ -92,7 +92,7 @@ mavenPublishing {
     publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates("dev.abbasian", "exoboost", "1.0.0")
+    coordinates("dev.abbasian", "exoboost", "1.0.1")
 
     pom {
         name.set("ExoBoost")
