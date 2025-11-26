1fcb8b3c7967ff53f019ab2fd4f36dd017e9a337
Abbasian.dev <mam-1371@outlook.com>
2025-11-26 13:31:01 +0330

    add compose material for add highlight bottom sheet

--- ExoBoost/build.gradle.kts
+++ ExoBoost/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
